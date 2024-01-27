314ed00de9eb8fcab13d50eb5706bfae199541e8
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-01-27 15:19:25 +0100

    feat: add simple POST method

--- nodev1/index.js
+++ nodev1/index.js
@@ -7,3 +7,21 @@ const cors = require('cors');
 // declare our app to use express
 const app = express();
 app.use(express.json());
+
+const legoSets = [];
+
+//var jsonContent = JSON.parse(contents);
+
+app.use(cors());
+ 
+app.post('/api/v1/legoSetId', (req, res) => {
+    const legoSet = {
+        legoSetId: req.body.legoSetId,
+    };
+    legoSets.push(legoSet);
+    res.send(`Lego Set ID: ${legoSet.legoSetId} is priced at $100.00`);
+});
+
+// Environment variable for port
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
